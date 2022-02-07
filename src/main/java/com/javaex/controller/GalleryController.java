379f3c7fe5e67c06aca8649e8107719be2ab31d5
황지우 <skyro13@naver.com>
2022-02-07 18:14:18 +0900

    gallery

--- src/main/java/com/javaex/controller/GalleryController.java
+++ src/main/java/com/javaex/controller/GalleryController.java
@@ -1,10 +1,24 @@
 package com.javaex.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.javaex.service.GalleryService;
 
 @Controller
 @RequestMapping("/gallery")
 public class GalleryController {
 
+	@Autowired
+	private GalleryService galleryService;
+	
+	public String list() {
+		System.out.println("GalleryController>list()");
+		
+		
+		
+		return "";
+	}
+	
 }
